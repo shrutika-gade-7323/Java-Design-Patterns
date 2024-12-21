bb9a2521c85ff4ac746f8fbca3c542e90249e525
shrutika-gade-7323 <shrutika.github@gmail.com>
2024-12-21 08:57:30 +0530

    Dependency Inversion Principle

--- 01. SOLID/05. D/Bad/Calculator.java
+++ 01. SOLID/05. D/Bad/Calculator.java
@@ -12,11 +12,9 @@ public class Calculator {
         int result = 0;
         switch (operation) {
             case "add":
-                AddOperation addOperation = new AddOperation();
-                result = addOperation.add(a, b);
+                result = a + b;
             case "sub":
-                SubOperation subOperation = new SubOperation();
-                result = subOperation.sub(a, b);
+                result = a - b;
         }
         return result;
     }
