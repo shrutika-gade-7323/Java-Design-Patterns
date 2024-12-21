c3410ae63e65bfd7dee7a29f0934644955ea23d8
shrutika-gade-7323 <shrutika.github@gmail.com>
2024-12-21 08:59:06 +0530

    Open/Closed Principle

--- 01. SOLID/02. O/OpenClosed.java
+++ 01. SOLID/02. O/OpenClosed.java
@@ -2,9 +2,8 @@ public class OpenClosed {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Operation add = new AddOperation();
-        Operation subtract = new SubtractOperation();
+        Operation sub = new SubtractOperation();
 
-        System.out.println("Addition: " + calculator.calculateNumber(1, 1, add));
-        System.out.println("Subtraction: " + calculator.calculateNumber(1, 1, subtract));
+        System.out.println("Addition: " + calculator.calculateNumber(1, 1, sub));
     }
 }
